ee246dbfbd517f4f8b32252738470a2667ac82ca
Michael-Burke <michael.burke.ang@Gmail.com>
2022-10-23 17:38:10 -0600

    Added Tkinter infromation and rendering the image

--- python_test.py
+++ python_test.py
@@ -1,4 +1,22 @@
 import random
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+
+root = Tk()
+
+# Create a photoimage object of the image in the path
+image1 = Image.open("./g_note.png")
+test = ImageTk.PhotoImage(image1)
+
+label1 = tkinter.Label(image=test)
+label1.image = test
+
+# Position image
+label1.place(x=-0.9, y=0.0)
+root.mainloop()
+
+
 
 
 a="a"
